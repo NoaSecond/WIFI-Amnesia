33db366098be54ed2a3baae8c22a52da31c8dad3
Noa Second <contact@noasecond.com>
2024-06-10 13:42:51 +0200

    :truck: Rename tmp files

--- WIFI-Amnesia.bat
+++ WIFI-Amnesia.bat
@@ -13,44 +13,44 @@ for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 
 :: Liste les profils wifi
 echo [Step 1/5] Listing wifi profiles...
-netsh wlan show profiles >> 0.txt
+netsh wlan show profiles >> 0amnesia.txt
 
 :: Pour chaque profil wifi, on affiche les informations
 echo [Step 2/5] Getting wifi profiles information...
 for /f "skip=9 tokens=1,2 delims=:" %%i in ('netsh wlan show profiles') do (
-    netsh wlan show profile %%j key=clear >> 1.txt
+    netsh wlan show profile %%j key=clear >> 1amnesia.txt
 )
 
-:: On nettoie et on garde les lignes qui contiennent " : " et on les met dans 2.txt
+:: On nettoie et on garde les lignes qui contiennent " : " et on les met dans 2amnesia.txt
 echo [Step 3/5] Cleaning wifi profiles information...
-for /f "delims=" %%k in ('findstr /c:" : " 1.txt') do (
-    echo %%k >> 2.txt
+for /f "delims=" %%k in ('findstr /c:" : " 1amnesia.txt') do (
+    echo %%k >> 2amnesia.txt
 )
 
-:: On lit la première ligne de 2.txt et on l'enregistre dans la variable firstLine
+:: On lit la première ligne de 2amnesia.txt et on l'enregistre dans la variable firstLine
 set "firstLine="
-for /f "delims=" %%a in (2.txt) do (
+for /f "delims=" %%a in (2amnesia.txt) do (
     set "firstLine=%%a"
     goto :doneReadingFirstLine
 )
 :doneReadingFirstLine
 
-:: On lit 2.txt, et à chaque fois qu'on croise une ligne égale à la variable firstLine, on rajoute une ligne "==================================" pour séparer les profils puis on enregistre dans 3.txt
+:: On lit 2amnesia.txt, et à chaque fois qu'on croise une ligne égale à la variable firstLine, on rajoute une ligne "==================================" pour séparer les profils puis on enregistre dans 3amnesia.txt
 setlocal enabledelayedexpansion
-for /f "delims=" %%a in (2.txt) do (
+for /f "delims=" %%a in (2amnesia.txt) do (
     if %%a==!firstLine! (
-        echo ================================== >> 3.txt
+        echo ================================== >> 3amnesia.txt
     ) else (
-        echo %%a >> 3.txt
+        echo %%a >> 3amnesia.txt
     )
 )
 
-:: On lit 3.txt et on met dans 4.txt les lignes 5 et 10 après chaque "=================================="
-:: ================== ISSUE : DANS 3.txt CERTAINS PROFILS N'ONT PAS DE MDP OU ALORS ON LES INFORMATIONS AUX MAUVAISES LIGNES ==================
+:: On lit 3amnesia.txt et on met dans 4amnesia.txt les lignes 5 et 10 après chaque "=================================="
+:: ================== ISSUE : DANS 3amnesia.txt CERTAINS PROFILS N'ONT PAS DE MDP OU ALORS ON LES INFORMATIONS AUX MAUVAISES LIGNES ==================
 echo [Step 4/5] Displaying wifi profiles information...
 set lineCount=0
 set profileCount=0
-for /f "delims=" %%b in (3.txt) do (
+for /f "delims=" %%b in (3amnesia.txt) do (
     if %%b==================================== (
         set /a profileCount+=1
         set lineCount=0
@@ -68,9 +68,9 @@ endlocal
 
 :: Nettoyage des fichiers temporaires
 echo [Step 5/5] Cleaning temporary files...
-del 0.txt
-del 1.txt
-del 2.txt
-del 3.txt
+del 0amnesia.txt
+del 1amnesia.txt
+del 2amnesia.txt
+del 3amnesia.txt
 
 pause
